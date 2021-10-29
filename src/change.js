b5535e2f8da35c67a5572abb9b12ecfc6fcf3520
Oluwafemi <femikola23@gmail.com>
2021-10-29 20:55:45 +0400

    Add Js files

---
+++ src/change.js
@@ -0,0 +1,9 @@
+export default function isComplete(i, listArray, isComp, descript) {
+  if (isComp.checked) {
+    listArray[i].completed = true;
+    descript.style.textDecoration = 'line-through';
+  } else {
+    listArray[i].completed = false;
+    descript.style.textDecoration = 'none';
+  } return listArray[i];
+}
---
+++ src/tasks.js
@@ -0,0 +1,50 @@
+export default class Task {
+  constructor(description, completed, index, id) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+    this.id = id;
+  }
+
+  static getTodos() {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem('todos'));
+    } return todos;
+  }
+
+  static addTask(task) {
+    const tasks = Task.getTodos();
+    tasks.push(task);
+    localStorage.setItem('todos', JSON.stringify(tasks));
+  }
+
+  static deleteTask(targetel) {
+    if (targetel.classList.contains('delete')) {
+      targetel.parentElement.parentElement.remove();
+    }
+  }
+
+  static removeTask(id) {
+    const todos = Task.getTodos();
+    todos.forEach((todo, index) => {
+      if (todo.id === id) {
+        todos.splice(index, 1);
+      }
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
+    window.location.reload();
+  }
+
+  static clearCompleted() {
+    const todos = Task.getTodos();
+    const newTodos = todos.filter((todo) => todo.completed !== true);
+    newTodos.forEach((object, index) => {
+      object.index = index + 1;
+    });
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+    window.location.reload();
+  }
+}
