ffcb8fdc36a0d7362457b2da90ab93faf274821b
Oluwafemi <femikola23@gmail.com>
2021-11-02 13:39:46 +0400

    Update deploy

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/to-do-list/",
+    publicPath: '/to-do-list/',
   },
   module: {
     rules: [
