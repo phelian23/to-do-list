861cf477abf017e53d8b43ab71f98813d923da18
Oluwafemi <femikola23@gmail.com>
2021-11-03 16:25:12 +0400

    Update js

--- src/change.js
+++ src/change.js
@@ -1,17 +1,9 @@
 export default function isComplete(i, listArray, isComp, descript) {
   if (isComp.checked) {
-    if (listArray[i].completed === true) {
-      listArray[i].completed = false;
-      descript.style.textDecoration = 'none';
-    } else {
-      listArray[i].completed = true;
-      descript.style.textDecoration = 'line-through';
-    }
-  } else if (listArray[i].completed === true) {
-    listArray[i].completed = false;
-    descript.style.textDecoration = 'none';
-  } else {
     listArray[i].completed = true;
     descript.style.textDecoration = 'line-through';
+  } else {
+    listArray[i].completed = false;
+    descript.style.textDecoration = 'none';
   }
 }
